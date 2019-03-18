1497483441022040723281c74590a21cb3c2c033
Abady <gamersspeaks@gmail.com>
2019-03-18 23:44:38 +0300

    chore: added an example

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,6 @@
 const blocksmc = require('../index');
 
 blocksmc.player('iAbady').then(player => {
-	console.log(player.games);
+	console.log(player.games.map(g => `${g.game} ${g.stats.Deaths}`));
+	console.log(player.rank);
 });
