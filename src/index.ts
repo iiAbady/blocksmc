a6deffff83551b2403ee3c508edeb2fefc1b127d
Abady <gamersspeaks@gmail.com>
2019-05-09 03:49:57 +0300

    chore(lint): switch to eslint

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import Blocks from './structures/Blocks';
-const { version }: { version: string } = require('../package.json');
+const { version }: { version: string } = require('../package.json'); // eslint-disable-line @typescript-eslint/no-var-requires
 
 export default Blocks;
 
 export {
-Blocks,
-version
+	Blocks, version
 };
 
--- src/structures/Blocks.ts
+++ src/structures/Blocks.ts
@@ -2,60 +2,63 @@ import fetch from 'node-fetch';
 import * as cheerio from 'cheerio';
 import { Player, Top } from '../responses/Responses';
 
-type DataOptions = {
-		type: 'player' | 'top',
-		username?: string | undefined,
-		game?: string | undefined
-};
+interface DataOptions {
+	type: 'player' | 'top';
+	username?: string | undefined;
+	game?: string | undefined;
+}
 
 export default class Blocks {
-		public async player(username: string): Promise<Player> {
-				if (typeof username !== 'string') throw new TypeError(`[BlocksMC] expected string on username, got ${typeof username}`);
-				return this._getData({type: 'player', username});
-				}
+	public async player(username: string): Promise<Player> {
+		if (typeof username !== 'string') throw new TypeError(`[BlocksMC] expected string on username, got ${typeof username}`);
+		return this._getData({ type: 'player', username });
+	}
 
-		public async top(game: string): Promise<Top> {
-				if (typeof game !== 'string') throw new TypeError(`[BlocksMC] expected string on game, got ${typeof game}`);
-				return this._getData({ type: 'top', game });
-				}
+	public async top(game: string): Promise<Top> {
+		if (typeof game !== 'string') throw new TypeError(`[BlocksMC] expected string on game, got ${typeof game}`);
+		return this._getData({ type: 'top', game });
+	}
 
-		private async _getData(options: DataOptions): Promise<any> {
-				if (options.type === 'player') {
-						const data: any = { games: [] };
-						const res = await fetch(`https://blocksmc.com/player/${options.username}`);
-						const rawData = await res.text();
-						const $ = cheerio.load(rawData);
-						const rank = $('.profile-rank').text().replace('\n', '')
-							.trim();
-						const timePlayed = $('h1[dir=ltr]').text().replace('\n', '')
-								.trim();
-						const name = $('.profile-header h1').text().trim();
-						Object.assign(data, { name });
-						Object.assign(data, { rank });
-						Object.assign(data, { timePlayed });
-						$('div.col-xl-4').each(function(this: any) {
-								const stats = {};
-								$(this).find('li').each(function(this: any) {
-										Object.assign(stats, { [$(this).find('div.key').text()]: $(this).find('div.val').text() });
-								});
-								data.games.push({
-										game: $(this).find('div.title').text()
-												.trim(),
-										stats
-								});
-						});
-						return data;
-								}
-
-				if (options.type === 'top') {
-					const data: any = [];
-					const res = await fetch(`https://blocksmc.com/${options.game}`);
-					const rawData = await res.text();
-					const $ = cheerio.load(rawData);
-					$('tbody').find('a').each(function(this: any) {
-						data.push($(this).text());
-					});
-					return data;
-			}
+	private async _getData(options: DataOptions): Promise<any> {
+		if (options.type === 'player') {
+			const data: any = { games: [] };
+			const res = await fetch(`https://blocksmc.com/player/${options.username}`);
+			const rawData = await res.text();
+			const $ = cheerio.load(rawData);
+			const rank = $('.profile-rank').text().replace('\n', '')
+				.trim();
+			const timePlayed = $('h1[dir=ltr]').text().replace('\n', '')
+				.trim();
+			const name = $('.profile-header h1').text().trim();
+			Object.assign(data, { name });
+			Object.assign(data, { rank });
+			Object.assign(data, { timePlayed });
+			// eslint-disable-next-line func-names
+			$('div.col-xl-4').each(function(this: any) {
+				const stats = {};
+				// eslint-disable-next-line func-names
+				$(this).find('li').each(function(this: any) {
+					Object.assign(stats, { [$(this).find('div.key').text()]: $(this).find('div.val').text() });
+				});
+				data.games.push({
+					game: $(this).find('div.title').text()
+						.trim(),
+					stats
+				});
+			});
+			return data;
 		}
+
+		if (options.type === 'top') {
+			const data: any = [];
+			const res = await fetch(`https://blocksmc.com/${options.game}`);
+			const rawData = await res.text();
+			const $ = cheerio.load(rawData);
+			// eslint-disable-next-line func-names
+			$('tbody').find('a').each(function(this: any) {
+				data.push($(this).text());
+			});
+			return data;
+		}
+	}
 }
